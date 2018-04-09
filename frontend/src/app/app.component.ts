8deac750103ac94ba9584a6a22c4f2e425544fb7
Matt Wagner <matt.a.wagner4@gmail.com>
2018-04-09 11:41:37 -0400

    Collect Users from Rails

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  users = [];
+  constructor(private http:HttpClient) {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/users')
+      .subscribe(res => {
+        this.users = res.users;
+      });
+  }
 }
